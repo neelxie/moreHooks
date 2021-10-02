69efd6ca515d294cc99c3b0118e6980ff96f72c6
neelxie <kidricederek@gmail.com>
2021-10-02 16:14:39 +0300

    Add useeffect hook

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
 import Ani from './components/Ani'
+import HookCount from './components/HookCount';
 
 function App() {
   return (
     <div className="App">
       {/* <Hoek /> */}
-      <Ani />
+      {/* <Ani /> */}
+      <HookCount />
       {/* <div>Derek</div> */}
     </div>
   );
---
+++ src/components/HookCount.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect} from 'react'
+
+function HookCount() {
+    const [count, setCount] = useState(0)
+    useEffect(() => {
+        document.title = `You clicked ${count} times`
+    })
+    return (
+        <div>
+            <button onClick={() => setCount(count + 1)}>Click {count} times</button>
+        </div>
+    )
+}
+
+export default HookCount
