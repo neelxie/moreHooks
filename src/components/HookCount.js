f6f0a88b30fe86c603b1ead507cca138717fb62b
neelxie <kidricederek@gmail.com>
2021-10-02 16:21:58 +0300

    Add useeffect hook

--- src/components/HookCount.js
+++ src/components/HookCount.js
@@ -5,9 +5,10 @@ function HookCount() {
     const [name, setName] = useState('') 
     useEffect(() => {
         document.title = `You clicked ${count} times`
-    })
+    }, [count])
     return (
         <div>
+            <input type='text' value={name} onChange={e => setName(e.target.value)} />
             <button onClick={() => setCount(count + 1)}>Click {count} times</button>
         </div>
     )
