f20f9f36d9f5d68d2045c1e2f4054adb55d61cd4
neelxie <kidricederek@gmail.com>
2021-10-04 19:39:17 +0300

    Add new response

--- src/components/FetchData.js
+++ src/components/FetchData.js
@@ -2,18 +2,24 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 function FetchData() {
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
   const [post, setPost] = useState([]);
 
   useEffect(() => {
-    axios.get(`https://jsonplaceholder.typicode.com/posts/${idFromInput}`)
-      .then(res => { setPost(res.data)} )
-      .catch(err => { console.log(err) })
-  }, [idFromInput])
+    axios
+      .get(`https://jsonplaceholder.typicode.com/posts/${idFromInput}`)
+      .then((res) => {
+        setLoading(true)
+        setPost(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [idFromInput]);
   return (
     <div>
-      <input type="text" value={id} onChange={e => setId(e.target.value)} />
+      <input type="text" value={id} onChange={(e) => setId(e.target.value)} />
       <button onClick={handleClick}>Fetch</button>
       <div>{post.title}</div>
       {/* <ul>
