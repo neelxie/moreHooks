6329275131760a494cb2ba1fda5f4d4aed359f0a
neelxie <kidricederek@gmail.com>
2021-10-04 13:04:15 +0300

    add changes

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,18 +1,18 @@
 import React, { useReducer } from "react";
 
 const initialState = {
-  firstCounter: 0 
-}
+  firstCounter: 0,
+};
 const reducer = (state, action) => {
   switch (action.type) {
     case "increment":
-      return { firstCounter: state.firstCounter + 1 }
+      return { firstCounter: state.firstCounter + 1 };
     case "decrement":
-      return { firstCounter: state.firstCounter - 1 }
+      return { firstCounter: state.firstCounter - 1 };
     case "reset":
-      return initialState
+      return initialState;
     default:
-      return state
+      return state;
   }
 };
 
@@ -21,9 +21,13 @@ function Counter() {
   return (
     <div>
       <div>Count - {count}</div>
-      <button onClick={() => dispatch("increment")}>Increment</button>
-      <button onClick={() => dispatch("decrement")}>Decrement</button>
-      <button onClick={() => dispatch("reset")}>Reset</button>
+      <button onClick={() => dispatch({ type: "increment", value: 1 })}>
+        Increment
+      </button>
+      <button onClick={() => dispatch({ type: "decrement", value: 1 })}>
+        Decrement
+      </button>
+      <button onClick={() => dispatch({ type: "reset" })}>Reset</button>
     </div>
   );
 }
